9d310f52818e196fbf4a99ef75a8d0df7b8ff8af
Tobias Hauser <git@tobiashauser.org>
2025-08-07 18:00:13 +0200

    [wip] general layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,11 @@
 import type { Component } from "solid-js";
 
 import "./App.css";
-import logo from "./logo.svg";
 
 const App: Component = () => {
   return (
-    <div>
-      <header>
-        <img src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class="bg-blue-200 p-10 h-screen w-full">
+      <div class="bg-red-300">Hello</div>
     </div>
   );
 };
