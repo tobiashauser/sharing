703b8d100317e88e881cca97ff78fbefc362d027
Tobias Hauser <git@tobiashauser.org>
2025-08-07 17:21:49 +0200

    [feat] tailwind + solid

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import './App.css';
+
+const App: Component = () => {
+  return (
+    <div class="bg-red-200">
+      <header>
+        <img src={logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
