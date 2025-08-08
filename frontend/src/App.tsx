b2665c82bda77e05b2f98d0a0cfb07195550d1fc
Tobias Hauser <git@tobiashauser.org>
2025-08-08 15:53:06 +0200

    [feat] add snapping to columns

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { type Component, createEffect } from "solid-js";
+import { type Component } from "solid-js";
 
 import "./App.css";
 import { createWindowDropzone } from "./components/window-drop-zone";
@@ -16,22 +16,11 @@ const App: Component = () => {
     setRefs(inputRef);
   });
 
-  createEffect(() => {
-    console.log("files", files());
-  });
-
-  createEffect(() => {
-    console.log("isDragging", isDragging());
-  });
-
-  createEffect(() => {
-    console.log("errors", errors());
-  });
-
   return (
-    // This div covers the entire viewport. It has the standard padding-2 on all sides.
     <>
+      {/* This is needed as a target for the drop zone. */}
       <input {...getInputProps()} class="sr-only" ref={inputRef} />
+      {/* This div covers the entire viewport. It has the standard padding-2 on all sides. */}
       <div class="flex justify-between">
         <span class="bg-red-200">ENTER CODE</span>
         <span class="bg-red-200">HELP</span>
@@ -40,23 +29,34 @@ const App: Component = () => {
       <div class="bg-yellow-200 mt-[min(max(100vw,40rem)-40rem,max(100vh,40rem)-40rem,20vh)]">
         <div class="flex justify-center">
           {/* Add enough margin for the shadow. */}
-          <span
-            onclick={() => console.log("Clicked me")}
-            class="m-6 bg-red-200"
-          >
-            ADD FILES
-          </span>
+          <span class="m-6 bg-red-200">ADD FILES</span>
         </div>
         {/* Set the width of each cell with `auto-cols'. */}
-        <div class="md:grid-flow-col md:grid-rows-5 gap-2 grid auto-cols-[minmax(100px,200px)] justify-center">
-          <span class="bg-green-200">FILE_1</span>
-          <span class="bg-green-200">FILE_2</span>
-          <span class="bg-green-200">FILE_3</span>
-          <span class="bg-green-200">FILE_4</span>
-          <span class="bg-green-200">FILE_5</span>
-          <span class="bg-green-200">FILE_6</span>
-          <span class="bg-green-200">FILE_7</span>
-          <span class="bg-green-200">FILE_8</span>
+        <div class="md:snap-x md:grid-flow-col md:grid-rows-5 gap-2 grid snap-mandatory auto-cols-[minmax(300px,400px)] justify-center-safe overflow-scroll [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+          <span class="bg-green-200 snap-start">FILE_1</span>
+          <span class="bg-green-200 snap-start">FILE_2</span>
+          <span class="bg-green-200 snap-start">FILE_3</span>
+          <span class="bg-green-200 snap-start">FILE_4</span>
+          <span class="bg-green-200 snap-start">FILE_5</span>
+          <span class="bg-green-200 snap-start">FILE_6</span>
+          <span class="bg-green-200 snap-start">FILE_7</span>
+          <span class="bg-green-200 snap-start">FILE_8</span>
+          <span class="bg-green-200 snap-start">FILE_1</span>
+          <span class="bg-green-200 snap-start">FILE_2</span>
+          <span class="bg-green-200 snap-start">FILE_3</span>
+          <span class="bg-green-200 snap-start">FILE_4</span>
+          <span class="bg-green-200 snap-start">FILE_5</span>
+          <span class="bg-green-200 snap-start">FILE_6</span>
+          <span class="bg-green-200 snap-start">FILE_7</span>
+          <span class="bg-green-200 snap-start">FILE_8</span>
+          <span class="bg-green-200 snap-start">FILE_1</span>
+          <span class="bg-green-200 snap-start">FILE_2</span>
+          <span class="bg-green-200 snap-start">FILE_3</span>
+          <span class="bg-green-200 snap-start">FILE_4</span>
+          <span class="bg-green-200 snap-start">FILE_5</span>
+          <span class="bg-green-200 snap-start">FILE_6</span>
+          <span class="bg-green-200 snap-start">FILE_7</span>
+          <span class="bg-green-200 snap-start">FILE_8</span>
         </div>
       </div>
     </>
