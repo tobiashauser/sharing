93cbd599fc2461efee629efb6e9ed13a0c0dc356
Tobias Hauser <git@tobiashauser.org>
2025-08-24 11:36:42 +0200

    [poc] delay action

--- client/Main.hs
+++ client/Main.hs
@@ -2,20 +2,37 @@
 
 module Main where
 
+import Language.Javascript.JSaddle (jsg, (#))
+import Control.Monad (void)
 import Miso
 
+delay :: Int -> action -> Transition model action
+delay ms action = withSink $ \sink -> do
+  win <- jsg ("window" :: MisoString)
+  cb  <- asyncCallback $ sink action
+  void $ win # ("setTimeout" :: MisoString) $ (cb, ms)
+
+-- | Model
 type Model = ()
 
-data Action = None
-
+-- | Action
+data Action = Hello { name :: String } 
+            | DelayedHello { name :: String }
+            
+-- | Reducer
 updateModel :: Action -> Transition Model Action
-updateModel _ = io_ $ consoleLog "Unreachable."
+updateModel (Hello name) = io_ $ consoleLog $ "Hello, " <> (ms name) <> "!"
+updateModel (DelayedHello name) = delay 3000 $ Hello name
 
+-- | View
 viewModel :: Model -> View Model Action
 viewModel () = p_ [] [ text "Hello, client!" ]
 
+-- | Main
 app :: App Model Action
-app = component () updateModel viewModel
+app = (component () updateModel viewModel) {
+  initialAction = Just $ DelayedHello "Mr. Blob"
+  }
 
 main :: IO () 
 main = run $ startApp app
