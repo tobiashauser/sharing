24d2681cc5453a5af4105cdd8809e30f45f8b12f
Tobias Hauser <git@tobiashauser.org>
2025-09-06 20:35:29 +0200

    [wip] styling

--- lib/sharing_web/components/drop_area.ex
+++ lib/sharing_web/components/drop_area.ex
@@ -4,8 +4,15 @@ defmodule SharingWeb.DropArea do
   def render(assigns) do
     ~H"""
     <div
-      class="h-40 max-w-md w-2/3 bg-surface rounded-xl"
+      class="mt-6 h-40 grid max-w-md w-2/3 rounded-xl shadow-[0px_0px_15px_3px_rgba(0,0,0,0.1)] dark:bg-elevated"
       phx-click="open-file-picker">
+      <div class="m-4 center-content">
+        <div class="border p-3 border-overlay rounded-full mb-3 center-content">
+          <.icon name="hero-arrow-up-tray" class="size-5 text-subtle" />
+        </div>
+        <p class="text-sm mb-1.5 font-medium">Upload files</p>
+        <p class="text-subtle text-xs">Drag & Drop or click to browser</p>
+      </div>  
     </div>
     """
   end
