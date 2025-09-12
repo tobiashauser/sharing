185c6b44a3b1e8b66bbd634b4dc0411ed38cf747
Tobias Hauser <git@tobiashauser.org>
2025-09-12 09:17:28 +0200

    refactor

--- lib/sharing_web/components/action_button.ex
+++ lib/sharing_web/components/action_button.ex
@@ -8,95 +8,84 @@ defmodule SharingWeb.ActionButton do
   ### View                                                                  ###
   ### --------------------------------------------------------------------- ###
 
-  attr(:class, :string, default: "")
-  attr(:contentClass, :string, default: "")
-
-  defp action_button_input(assigns) do
+  defp input(assigns) do
     ~H"""
-    <div
-      id="ab-input"
-      class={"center-content " <> @class} >
+    <div id="ab-input" class="center-content w-0">
       <input
-        class={@contentClass}
         id="ab-input-content"
         type="text"
         placeholder="Enter Code"
-      /> 
+        class={"transition py-1 px-2 invisible opacity-0"
+          <> " focus:outline-none"
+          <> " invalid:text-critical"} /> 
     </div>
     """
   end
 
-  attr(:class, :string, default: "")
-  attr(:contentClass, :string, default: "")
-
-  defp action_button_download(assigns) do
+  defp download(assigns) do
     ~H"""
     <div
       id="ab-download"
-      class={"center-content" <> " " <> @class}>
+      class={"center-content w-0 cursor-pointer text-subtle transition bg-surface/60"
+        <> " hover:text-foreground hover:not-invalid:bg-surface"
+        <> " invalid:bg-critical/60"}>
       <span
         id="ab-download-content"
-        class={"hero-arrow-down-tray-micro" <> " " <> @contentClass}
-      />
+        class={"hero-arrow-down-tray-micro focus:outline-none mx-2 invisible opacity-0"
+          <> " focus:outline-none"
+          <> " transition invalid:text-critical"} />
     </div>
     """
   end
 
-  attr(:class, :string, default: "")
-  attr(:contentClass, :string, default: "")
-
-  defp action_button_enter_code(assigns) do
+  defp button(assigns) do
     ~H"""
     <button
       id="ab-enter-code"
-      class={"center-content " <> @class}
+      class={"center-content text-subtle cursor-pointer"
+        <> " hover:text-foreground hover:bg-surface"
+        <> " not-dark:bg-surface/60"}
       phx-click="show-input">
       <span
         id="ab-enter-code-content"
-        class={@contentClass}>
+        class="px-2">
         Enter Code
       </span>
     </button>
     """
   end
 
-  attr(:class, :string, default: "")
-  attr(:contentClass, :string, default: "")
-
-  defp action_button_code_label(assigns) do
+  defp label(assigns) do
     ~H"""
     <div
       id="ab-code-label"
-      class={"center-content " <> @class}>
+      class="center-content bg-sruface/60 w-0">
       <span
         id="ab-code-label-content"
-        class={@contentClass}>
+        class={"invisible opacity-0 px-2 font-normal text-foreground/80"
+          <> " focus:outline-none"}>
         Code
       </span>
     </div>
     """
   end
 
-  attr(:class, :string, default: "")
-  attr(:contentClass, :string, default: "")
-  attr(:code, :string, required: true)
+  attr(:petname, :string, required: true)
 
-  defp action_button_code(assigns) do
+  defp code(assigns) do
     ~H"""
-    <div
-      id="ab-code"
-      class={"center-content " <> @class}>
+    <div id="ab-code" class="center-content w-0">
       <span
         id="ab-code-content"
-        class={@contentClass}>
-        {@code}
+        class={"invisible opacity-0 px-2 text-popout"
+          <> " focus:outline-none"}>
+        {@petname}
       </span>
     </div>
     """
   end
 
-  attr(:class, :string, default: "")
-  attr(:code, :string, default: "No code provided")
+  attr(:petname, :string, default: "No code provided")
 
   def render(assigns) do
     ~H"""
@@ -104,37 +93,13 @@ defmodule SharingWeb.ActionButton do
       id="action-button"
       phx-hook="ActionButtonEvents"
       class="flex justify-start font-medium text-sm">
-      <div class={"inline-flex truncate text-clip border transition border-surface/60 rounded invalid:border-critical" <> " " <> @class}>
-        <.action_button_input
-          class="w-0"
-          contentClass="focus:outline-none transition py-1 px-2 invisible opacity-0 invalid:text-critical"
-        />
-        <.action_button_download
-          class={
-            "w-0 cursor-pointer text-subtle transition"
-            <> " bg-surface/60"
-            <> " hover:text-foreground hover:not-invalid:bg-surface"
-            <> " invalid:bg-critical/60"
-          }
-          contentClass="focus:outline-none mx-2 invisible opacity-0 transition invalid:text-critical"
-        />
-        <.action_button_enter_code
-          class={
-            "text-subtle cursor-pointer"
-            <> " not-dark:bg-surface/60"
-            <> " hover:text-foreground hover:bg-surface"
-          }
-          contentClass="px-2"
-        />
-        <.action_button_code_label
-          class="bg-surface/60 w-0"
-          contentClass="invisible opacity-0 focus:outline-none px-2 font-normal text-foreground/80"
-        />
-        <.action_button_code
-          class="w-0"
-          contentClass="invisible opacity-0 focus:outline-none px-2 text-popout"
-          code={@code}
-        />
+      <div class={"inline-flex truncate text-clip border transition border-surface/60 rounded"
+        <> " invalid:border-critical"}>
+        <.input />
+        <.download />
+        <.button />
+        <.label />
+        <.code petname={@petname} />
       </div>
     </div>
     """
